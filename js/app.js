ae3fc75cc958a32eaeb35e6122c0d7a8f445f207
Lautaro Porlan <l.porlann@gmail.com>
2022-10-05 02:03:19 -0300

    se agrega func async, se separan archivos js

--- js/app.js
+++ js/app.js
@@ -1,236 +1,127 @@
-// CONEXION DOM
-
-const productCard = document.querySelector("#product-card");
-const filtred = document.querySelector("#buscador");
-const cartCard = document.querySelector("#cart-card");
-const cartTable = document.querySelector("#cart-table");
-
-
-// EVENTOS
 
 
 
-// PRODUCTOS
+//DOM CONTENT LOADED----------------
 
-const products = [
-  {
-    id: 1,
-    name: "JOHNNIE WALKER BLACK",
-    price: 8000,
-    stock: 20,
-    category: "whiskey",
-    img: "/images/whiskey/johnnie-black-png.png",
-  },
-  {
-    id: 2,
-    name: "JOHNNIE WALKER RED",
-    price: 6000,
-    stock: 40,
-    category: "whiskey",
-    img: "/images/whiskey/jonnie-red-png.png",
-  },
-  {
-    id: 3,
-    name: "JOHNNIE DOUBLE BLACK",
-    price: 15000,
-    stock: 10,
-    category: "whiskey",
-    img: "/images/whiskey/johnnie-doble-black.png",
-  },
-  {
-    id: 4,
-    name: "JOHNNIE WALKER SWING",
-    price: 8000,
-    stock: 15,
-    category: "whiskey",
-    img: "/images/whiskey/johnnie-swing.png",
-  },
-  {
-    id: 5,
-    name: "JACK DANIELS N°7",
-    price: 8000,
-    stock: 15,
-    category: "whiskey",
-    img: "/images/whiskey/jack-daniels.png",
-  },
-  {
-    id: 6,
-    name: "THE SINGLETON 18",
-    price: 8000,
-    stock: 15,
-    category: "whiskey",
-    img: "/images/whiskey/the-singleton-18.png",
-  },
-];
+document.addEventListener("DOMContentLoaded", async ()=> {
+  const wait = await loadDB()
+                     setCartButtons() 
+        
+})
 
-// CARRITO DE COMPRAS
 
-const carrito = [];
 
-// MOSTRAR PRODUCTOS
 
-const displayProducts = (array) => {
-  let card = "";
-  productCard.innerHTML = "";
 
-  array.forEach((product) => {
-    card += `<div class="col-lg-3 text-center">
-    <div class="card border-0 bg-light mb-2">
-        <div class="card-body">
-            <img src= ${product.img} class="img-fluid" alt="Imagen jack-daniels">
-        </div>
-    </div>
-    <h6>${product.name}</h6>
-    <p>$${product.price}</p>
-    <button type="submit" class="btn-carrito" id=${product.id}>Agregar al carrito</button>
-    
 
-</div>`;
-    productCard.innerHTML = card;
-  });
-};
-displayProducts(products);
+// CONEXION CON DOM---------
 
-// FILTRAR PRODUCTOS
 
-const filterProducts = () => {
-  filtred.value = filtred.value.trim().toUpperCase();
-  if (filtred.value !== "") {
-    const result = products.filter((product) =>
-      product.name.includes(filtred.value)
-    );
-    if (result.length === 0) {
-      displayWhiskey(products);
-    } else {
-      let card = "";
-      result.forEach((element) => {
-        card += `<div class="col-lg-3 text-center">
-    <div class="card border-0 bg-light mb-2">
-        <div class="card-body">
-            <img src= ${element.img} class="img-fluid" alt="Imagen jack-daniels">
-        </div>
-    </div>
-    <h6>${element.name}</h6>
-    <p>${element.price}</p>
-    <button type="submit" class="btn-carrito" id=btn-${element.id}>Agregar al carrito</button>
 
-    </div>`;
-        productCard.innerHTML = card;
-      });
-    }
-  } else {
-    displayProducts(products);
-  }
-};
 
-filtred.addEventListener("#buscador", filterProducts());
-filtred.addEventListener("keypress", (e) => {
-  if (e.key === "Enter") {
-    filterProducts();
-  }
-});
 
-// CARRITO FUNCTIONS
 
-const removeCartItem = () =>{
-  const deleteButton = document.querySelectorAll(".btn-eliminar-carrito")
-  for (const button of deleteButton) {
-    button.addEventListener('click' , () =>{
-      let itemToRemove = carrito.findIndex(item =>
-        item.id == button.id
-      ) 
-     if(itemToRemove !== -1){
-      deleteConfirm()
-      carrito.splice( itemToRemove , 1)
-      setLocalCarrito()
-      displayCarrito(carrito)
-     }else{
-      displayCarrito(carrito)
-     }
+// cart FUNCTIONS
+
+// const removeCartItem = () =>{
+//   const deleteButton = document.querySelectorAll(".btn-eliminar-cart")
+//   for (const button of deleteButton) {
+//     button.addEventListener('click' , () =>{
+//       let itemToRemove = cart.findIndex(item =>
+//         item.id == button.id
+//       ) 
+//      if(itemToRemove !== -1){
+//       deleteConfirm()
+//       cart.splice( itemToRemove , 1)
+//       setLocalcart()
+//       displaycart(cart)
+//      }else{
+//       displaycart(cart)
+//      }
         
       
     
-      })
-    }
-}
+//       })
+//     }
+// }
 
 
 
-const displayCarrito = (array) => {
+// const displayCart = (array) => {
   
-  let card = "";
-  cartTable.innerHTML = "";
+//   let card = "";
+//   cartTable.innerHTML = "";
 
-  array.forEach((product) => {
+//   array.forEach((product) => {
  
-  card = `
+//   card = `
   
-  <tr>
+//   <tr>
   
-    <td>${product.name}</td>
-    <td><img src=${product.img} class="cart-img" alt=""></td>
-    <td>$${product.price}</td>
-    <td>${product.quantity}</td>
-    <td><button type="submit" class="btn-eliminar-carrito" id="${product.id}" product-data="${product.id}" ><i class="fa-solid fa-trash"></i></button></td>
+//     <td>${product.name}</td>
+//     <td><img src=${product.img} class="cart-img" alt=""></td>
+//     <td>$${product.price}</td>
+//     <td>${product.quantity}</td>
+//     <td><button type="submit" class="btn-eliminar-carrito" id="${product.id}" product-data="${product.id}" ><i class="fa-solid fa-trash"></i></button></td>
     
     
-  </tr>  
+//   </tr>  
   
 
-</table>`
+// </table>`
 
 
 
-    cartTable.innerHTML += card;
-    cartTotal()
-    removeCartItem()
+//     cartTable.innerHTML += card;
+//     cartTotal()
+//     removeCartItem()
      
-  });
+//   });
   
-};
+// };
 
-const cartButton = () => {
-  let buttonCart = document.querySelectorAll(".btn-carrito");
-  for (  const button of buttonCart) {
-    button.addEventListener("click", () => {
-      let prod = carrito.find((product) => product.id == button.id);
-      if (prod) {
-        prod.quantity++;
-      } else {
-        let prod = products.find((product) => product.id == button.id);
-        if (prod) {
-          let newProduct = {
+// const cartButton = () => {
+//   let buttonCart = document.querySelectorAll(".btn-carrito");
+//   for (  const button of buttonCart) {
+//     button.addEventListener("click", () => {
+//       let prod = carrito.find((product) => product.id == button.id);
+//       if (prod) {
+//         prod.quantity++;
+//       } else {
+//         let prod = products.find((product) => product.id == button.id);
+//         if (prod) {
+//           let newProduct = {
             
-            ...prod,
-            quantity: 1,
-          };
-          cartTotal()
-          carrito.push(newProduct);
+//             ...prod,
+//             quantity: 1,
+//           };
+//           cartTotal()
+//           carrito.push(newProduct);
           
-        }
-      }
+//         }
+//       }
       
-      displayCarrito(carrito);
-      removeCartItem()
+//       displayCarrito(carrito);
+//       removeCartItem()
       
-      setLocalCarrito()
-      cartTotal()
+//       setLocalCarrito()
+//       cartTotal()
       
       
       
-    });
-  }
-};
+//     });
+//   }
+// };
 
-cartButton();
+// cartButton();
 
 const cartTotal =() =>{
   
   let total = 0
   
-  const displayCartTotal = document.querySelector(".total-value")
-  if(carrito.length > 0){
-  carrito.forEach(item =>{
+  const displayCartTotal = document.querySelector("#total-value")
+  if(cart.length > 0){
+  cart.forEach(item =>{
     const cartPrice = Number(item.price)
     total = total + cartPrice*item.quantity
     displayCartTotal.innerHTML = ` TOTAL = ${total}`
@@ -238,15 +129,16 @@ const cartTotal =() =>{
   })}
 }    
 
-const deleteConfirm = () =>{
-  Swal.fire({
-    icon: 'success',
-    title: 'Eliminado',})
 
-    }
       
     
-  
+  // STORAGE FUNCTIONS
+
+
+const setLocalCart = () =>{  localStorage.setItem("cart" , JSON.stringify(cart))}
+const localCart = JSON.parse(localStorage.getItem("cart"))    
+
+
 
   
 
@@ -278,14 +170,6 @@ const deleteConfirm = () =>{
                                     //STORAGE FUNCTIONS
 
 
-const setLocalCarrito = () =>{  localStorage.setItem("carrito" , JSON.stringify(carrito))}
-    
-const localCarrito = JSON.parse(localStorage.getItem("carrito"))
-window.onload = function(){
-    
-    localCarrito && localCarrito.forEach(item =>{carrito.push(item)})
-    displayCarrito(carrito)
-}
     
 
 
--- js/functions.js
+++ js/functions.js
@@ -1,1 +1,97 @@
-const url = '../db/products.json'
+// PRODUCTS Y DATABASE-------
+const url = "../db/products.json";
+let cart = [];
+let products = [];
+
+// DOM PRODUCTS
+const productCard = document.querySelector("#product-card");
+const cartTable = document.querySelector("#cart-table");
+let card = "";
+
+
+
+// CARGA DESDE DATABASE Y MOSTRAR PRODUCTOS
+
+const loadDB = async () => {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    products = data;
+    products.forEach((element) => (card += displayProducts(element)));
+  } catch (e) {
+    console.log(e);
+  } finally {
+    productCard.innerHTML = card;
+  }
+};
+
+const displayProducts = (db) => {
+  const { id, img, name, price } = db;
+
+  return `<div class="col-lg-3 text-center">
+                <div class="card border-0 bg-light mb-2">
+                <div class="card-body">
+                <img src= ${img} class="img-fluid" alt="Imagen jack-daniels">
+                </div>
+                </div>
+                <h6>${name}</h6>
+                <p>$${price}</p>
+                <button type="submit" class="btn-carrito" id=${id}>Agregar al carrito</button>
+          </div>`;
+};
+
+// CARRITO FUNCTIONS------------------------------------
+
+const setCartButtons = () => {
+  let cartButton = document.querySelectorAll(".btn-carrito");
+  cartButton.forEach((button) =>
+    button.addEventListener("click", (e) => addToChart(e))
+  );
+};
+
+const displayCart = (array) => {
+  let cartCard = "";
+  cartTable.innerHTML = "";
+
+  array.forEach((product) => {
+    cartCard = `
+    
+    <tr>
+    
+      <td>${product.name}</td>
+      <td><img src=${product.img} class="cart-img" alt=""></td>
+      <td>$${product.price}</td>
+      <td>${product.quantity}</td>
+      <td><button type="submit" class="btn-eliminar-carrito" id="${product.id}"  ><i class="fa-solid fa-trash"></i></button></td>
+      
+      
+    </tr> 
+    
+  
+  </table>`;
+
+    cartTable.innerHTML += cartCard;
+  });
+};
+
+const addToChart = (event) => {
+  let prod = cart.find((item) => item.id == parseInt(event.target.id));
+  if (prod) {
+    prod.quantity++;
+  } else {
+    let prod = products.find((item) => item.id == parseInt(event.target.id));
+    if (prod) {
+      let newProduct = {
+        ...prod,
+        quantity: 1,
+      };
+      cart.push(newProduct);
+
+      setLocalCart();
+      displayCart(cart);
+      cartTotal()
+      
+    }
+  }
+};
+
