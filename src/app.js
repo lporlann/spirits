f10d4bdc7d7cc968f17edc259954a523084eb7e0
Lautaro Porlan <l.porlann@gmail.com>
2023-03-07 23:34:15 -0300

    node js update

--- src/app.js
+++ src/app.js
@@ -1,19 +1,26 @@
-import express from "express"
+import express from "express";
+import morgan from "morgan";
 import { dirname , join } from "path";
 import { fileURLToPath } from "url";
-import indexRoutes from './router/index.js'
+import indexRoutes from './router/index.js';
 
 const app = express();
-const __dirname = dirname(fileURLToPath(import.meta.url))
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
-app.set('views' , join(__dirname , 'views'))
-app.set('view engine' , 'ejs')
-app.use(indexRoutes)
-app.use(express.static(join(__dirname , 'public')))
+app.set('views' , join(__dirname , 'views'));
+app.set('view engine' , 'ejs');
+app.use(indexRoutes);
+app.use(express.static(join(__dirname , 'public')));
+
+// middlewares
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended : false}));
+app.use(express.json());
 
 
 
+const port = app.listen(3000);
+console.log(`Server is listening on port 3000`)
 
 
-const PORT = app.listen(3000);
-console.log(`Server is listening on 3000`)
+//NPM RUN DEV//
